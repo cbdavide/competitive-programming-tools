1318b039154c71b66ca4fa8489d335381bcf4968
David Castelblanco Benavides <cbdavides@gmail.com>
2019-01-15 23:30:48 -0500

    Added function to download a determined list of problems

--- codeforces-scraper/src/main.py
+++ codeforces-scraper/src/main.py
@@ -4,6 +4,7 @@ from concurrent.futures import ThreadPoolExecutor
 from src.os import create_folder
 from src.os import save_problem_cases
 
+from src.util import Problem
 from src.request import get_html
 
 from src.scraping import get_test_cases
@@ -44,3 +45,25 @@ def create_contest(contest_id):
 
     with ThreadPoolExecutor(max_workers=5) as executor:
         executor.map(download_cases, problem_list)
+
+
+def create_problems(plain_problems):
+
+    def to_problem_list():
+
+        for plain in plain_problems:
+            contest, id = plain.split('/')
+            url = "/problemset/problem/{0}".format(
+                plain,
+                CODEFORCES_BASE_URL
+            )
+
+            yield Problem(id, url, contest)
+
+    problems = list(to_problem_list())
+
+    for p in problems:
+        create_folder(p.contest)
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(download_cases, problems)

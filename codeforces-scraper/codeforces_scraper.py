b4facb8972182b0f4e5dbefa5bfa79b0ab61ab26
David Castelblanco Benavides <cbdavides@gmail.com>
2019-01-19 00:04:49 -0500

    Declared options in the CLI to pass the output path of the process and a template file.

--- codeforces-scraper/codeforces_scraper.py
+++ codeforces-scraper/codeforces_scraper.py
@@ -7,33 +7,53 @@ It has two modes of use, it can be used to download a determined set of
 problems or it can download all the problems of a contest.
 
 Usage:
-    codeforces_scraper contest <contest_id>
-    codeforces_scraper problem <problem_id>...
+    codeforces_scraper contest <contest_id> [--output=<dir>] [--template=<templ>]
+    codeforces_scraper problem <problem_id>... [--output=<dir>] [--template=<templ>]
     codeforces_scraper (-h | --help)
 
 Example:
     codeforces_scraper contest 1097
     codeforces_scraper problem 1036/C 1081/C
-
+    codeforces_scraper contest 1097 --template=tpl.cpp
+    codeforces_scraper contest 1097 --output=/home/user/competitive-programming
 """
 
 import sys
 from docopt import docopt
+from src.os import exists
 from src.main import create_contest
 from src.main import create_problems
 
 
-def cli():
-    arguments = docopt(__doc__)
+def cli(arguments):
+
+    def validate():
+        '''
+            This function raises an exception in case that the arguments
+            are not consistent.
+        '''
+
+        if arguments['--output'] and not exists(arguments['--output']):
+            raise Exception('Output path does not exist...')
+
+        if arguments['--template'] and not exists(arguments['--template']):
+            raise Exception('Template file does not exist...')
+
+    validate()
+
+    options = {}
+    if arguments['--output']:
+        options['base_path'] = arguments['--output']
 
     if arguments['contest'] and arguments['<contest_id>']:
-        create_contest(arguments['<contest_id>'])
+        create_contest(arguments['<contest_id>'], **options)
         sys.exit(0)
 
     if arguments['problem'] and arguments['<problem_id>']:
-        create_problems(arguments['<problem_id>'])
+        create_problems(arguments['<problem_id>'], **options)
         sys.exit(0)
 
 
 if __name__ == '__main__':
-    cli()
+    arguments = docopt(__doc__)
+    cli(arguments)
